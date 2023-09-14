101301b6fba4c6fffb7944bb7144f5043fb8fb89
Samara Luiza Cunha Silva <106708531+samaralcunha@users.noreply.github.com>
2023-09-14 11:31:41 -0300

    egsfgsreh

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,26 +4,37 @@ import Card from './components/card'
 
 function App() {
   const [dinos, setDinos] = useState([])
+  const [filtro, setFiltro] = useState("dinos")
 
   useEffect(() => {
     (async () => {
-      const response = await fetch('http://localhost:4000/dinos');
+      console.log(filtro)
+      const response = await fetch(`http://localhost:4000/${filtro}`);
       const data = await response.json();
       setDinos(data);
     })()
-  }, []);
+  }, [filtro]);
 
   return (
     <>
-      <div>
+      <div className="min-h-screen justify-center items-center flex flex-col flex-wrap gap-8 p-8 bg-[#40916c] ">
+        <div className="flex flex-wrap gap-8">
+          <button className="w-32 border-double border-4 rounded-sm bg-[#52b69a] shadow-lg font-period text-lg" onClick={() => setFiltro("dinos")}>Todos</button>
+          <button className="w-32 border-double border-4 rounded-sm bg-[#52b69a] shadow-lg font-period text-lg" onClick={() => setFiltro("Jurassico")}>Jurássico</button>
+          <button className="w-32 border-double border-4 rounded-sm bg-[#52b69a] shadow-lg font-period text-lg" onClick={() => setFiltro("Cretaceo")}>Cretáceo</button>
+          <button className="w-32 border-double border-4 rounded-sm bg-[#52b69a] shadow-lg font-period text-lg" onClick={() => setFiltro("Triassico")}>Triássico</button>
+        </div>
+        <div className="flex flex-wrap gap-8 p-8 justify-center items-center" >
         {dinos.map((dino) => (
           <Card
+            key={dino.id}
             nome={dino.nome}
             nomeCien={dino.nomeCien}
             periodo={dino.periodo}
             imagem={dino.imagem}
           />
         ))}
+        </div>
       </div>
     </>
   )
--- react/src/components/card.jsx
+++ react/src/components/card.jsx
@@ -1,14 +1,18 @@
 export default function Card({ nome, nomeCien, periodo, imagem }) {
     return (
         <>
-            <section>
-                <div>
-                    <h1>{nome}</h1>
-                    <h2>{nomeCien}</h2>
-                    <h3>{periodo}</h3>
-                    <div style="background-image: url('{imagem}')"></div>
+            <div className="w-1/4 bg-white shadow-lg rounded-lg overflow-hidden bg-[#76c893]">
+                <div className="relative">
+                    <img className="w-full h-32 object-cover brightness-50 blur-[1px]" src={imagem} alt="Imagem de fundo" />
+                    <div className="absolute inset-0 flex items-end p-4">
+                        <h2 className="text-white text-3xl font-semibold text-[#99d98c] font-title">{nome}</h2>
+                    </div>
                 </div>
-            </section>
+                <div className="p-4">
+                    <p className="text-gray-700  font-period text-lg">{nomeCien}</p>
+                    <p className="text-gray-700 font-description text-base">{periodo}</p>
+                </div>
+            </div>
         </>
     )
 }
